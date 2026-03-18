08cb12f7d495cdaa66a2e264422b5bd8f84cc30a
suyog khadka <ksuyog697@gmail.com>
2026-03-18 23:24:01 +0545

    change the forntend

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -125,9 +125,9 @@ MEDIA_URl='/media/'
 
 
 
-EMAIL_BACKEND = config('EMAIL_BACKEND')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'ksuyog697@gmail.com'
+EMAIL_HOST_PASSWORD = 'dnms gift pjpm yqva'
